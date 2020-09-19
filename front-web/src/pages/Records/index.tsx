87af9548ef4af94f4b3ebae80b4f88259f8aeda8
Michel Rosenblat <michelrosenblat@gmail.com>
2020-09-18 21:01:31 -0300

    Tabela de records pronta

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import './styles.css';
 
 const Records = () => (
-    <h1>Records</h1>
+    <div className="page-container">
+        <table className="records-table" cellPadding="0" cellSpacing="0">
+            <thead>
+                <tr>
+                    <th>INSTANTE</th>
+                    <th>NOME</th>
+                    <th>IDADE</th>
+                    <th>PLATAFORMA</th>
+                    <th>GÊNERO</th>
+                    <th>TÍTULO DO GAME</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>2020/08/2020</td>
+                    <td>Washington Soares</td>
+                    <td>25</td>
+                    <td>XBOX</td>
+                    <td>Ação - Aventura</td>
+                    <td>The Last of Us</td>
+                </tr>
+                <tr>
+                    <td>2020/08/2020</td>
+                    <td>Washington Soares</td>
+                    <td>25</td>
+                    <td>XBOX</td>
+                    <td>Ação - Aventura</td>
+                    <td>The Last of Us</td>
+                </tr>
+                <tr>
+                    <td>2020/08/2020</td>
+                    <td>Washington Soares</td>
+                    <td>25</td>
+                    <td>XBOX</td>
+                    <td>Ação - Aventura</td>
+                    <td>The Last of Us</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 );
 
 export default Records;
